abdc4291f18480a100c604e7b18daf8c9d4da9f0
ladejib <ladejibiobaku@gmail.com>
2025-04-16 12:52:19 -0400

    delete cypress under workflow

--- .github/workflows/cypress/cypress.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:5000', // Matches your Docker app port
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
-  }
-})
--- .github/workflows/cypress/e2e/login.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Login Tests', () => {
-  it('Successfully logs in', () => {
-    cy.visit('/')
-    cy.get('input[name="username"]').type('admin')
-    cy.get('input[name="password"]').type('admin123')
-    cy.get('button[type="submit"]').click()
-    cy.contains('Welcome, admin').should('be.visible')
-  })
-})
